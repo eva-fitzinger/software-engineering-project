f02188c3e7b204896f076a58c7fcfa9b04736ba8
werner <jw_koeck@hotmail.com>
2020-12-12 16:21:33 +0100

    Enable scheduling

--- city-traffic-control/configuration/src/main/java/configuration/Application.java
+++ city-traffic-control/configuration/src/main/java/configuration/Application.java
@@ -3,8 +3,11 @@ package configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = {"at.jku.softengws20.group1"})
+@EnableScheduling
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
