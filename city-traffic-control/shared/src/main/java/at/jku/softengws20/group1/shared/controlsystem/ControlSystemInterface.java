79e05c994657684768190f730a48cf7e570d01d6
Jakob Wögerbauer <jakob.woegerbauer@gmx.at>
2021-01-18 22:22:53 +0100

    add API docs for ControlSystemInterface

--- city-traffic-control/shared/src/main/java/at/jku/softengws20/group1/shared/controlsystem/ControlSystemInterface.java
+++ city-traffic-control/shared/src/main/java/at/jku/softengws20/group1/shared/controlsystem/ControlSystemInterface.java
@@ -4,14 +4,35 @@ public interface ControlSystemInterface<T extends MaintenanceRequest> {
     String URL = "/controlsystem";
 
     String GET_ROAD_NETWORK_URL = "roadNetwork";
+
+    /**
+     * This method provides a valid and in itself consistent RoadNetwork.
+     * @return the roadNetwork that all subsystems fetch at startup
+     */
     RoadNetwork getRoadNetwork();
 
     String GET_STATUS_URL = "status";
+
+    /**
+     * Provides a RoadSegmentStatus for every RoadSegment that exists in the RoadNetwork
+     * which can be retrieved with getRoadNetwork()
+     * @return an array of RoadSegmentStatus of length N, N being the number of RoadSegments in the RoadNetwork.
+     */
     RoadSegmentStatus[] getStatus();
 
     String REQUEST_ROAD_CLOSING_URL = "requestRoadBlocking";
+
+    /**
+     * Submit a MaintenanceRequest for review or direct action (for emergency requests)
+     * @param request A MaintenanceRequest with an id != null and a valid RoadSegmentId
+     */
     void requestRoadClosing(T request);
 
     String SET_ROAD_AVAILABLE_URL = "setRoadAvailable";
+
+    /**
+     * Notifies the ControlCenter, that a previously closed road (by MaintenanceRequest or manual closing) is reopened.
+     * @param roadSegmentId A valid id of an existing RoadSegment in the RoadNetwork
+     */
     void setRoadAvailable(String roadSegmentId);
 }
